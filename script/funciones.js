f92b634814fdb80606b9f7aa7612e673b85f45a7
EmilianoMoro9 <emimoro2003@gmail.com>
2023-12-14 13:38:55 -0300

    pagina personal web sin react

---
+++ script/funciones.js
@@ -0,0 +1,12 @@
+let html = document.getElementById('boton1');
+let modificar = document.getElementById('modificar');
+let proyec = document.getElementById('proyec');
+
+boton1.addEventListener('click',() => {
+    location.href = 'https://lenguajehtml.com/';
+})
+
+
+proyec.addEventListener('click', () => {
+    location.href = '../pages/proyects.html'
+})
